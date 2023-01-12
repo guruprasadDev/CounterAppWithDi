8640095f21418a18bb41b3603183f5ebbc239b50
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2023-01-12 13:42:30 +0530

    MainActivity.kt add btn and text

--- app/src/main/java/com/example/counterappwithdi/MainActivity.kt
+++ app/src/main/java/com/example/counterappwithdi/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.counterappwithdi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.counterappwithdi.databinding.ActivityMainBinding
+import com.example.counterappwithdi.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var viewModel: MainViewModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModel = ViewModelProvider(this)[(MainViewModel::class.java)]
+
+        binding.countBtn.setOnClickListener {
+            binding.countTv.text = viewModel?.count.toString()
+            viewModel?.updateCount()
+        }
     }
 }
