df2738078d66805b93716a0f6b98ce2fc1cef188
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2023-01-13 11:36:14 +0530

    update ViewModelFactory.kt

--- app/src/main/java/com/example/counterappwithdi/MainActivity.kt
+++ app/src/main/java/com/example/counterappwithdi/MainActivity.kt
@@ -5,17 +5,22 @@ import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.example.counterappwithdi.databinding.ActivityMainBinding
 import com.example.counterappwithdi.viewmodel.MainViewModel
+import com.example.counterappwithdi.viewmodel.ViewModelFactory
+import java.time.chrono.Chronology.of
+import java.util.EnumSet.of
+import java.util.Map.of
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var viewModel: MainViewModel? = null
+
+    var viewModel: MainViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        viewModel = ViewModelProvider(this)[(MainViewModel::class.java)]
-
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         binding.countBtn.setOnClickListener {
             binding.countTv.text = viewModel?.count.toString()
             viewModel?.updateCount()
