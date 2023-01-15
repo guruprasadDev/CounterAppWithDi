cacffdaa9893961ffb9f16a97628a20033898ea6
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2023-01-15 20:59:41 +0530

    ViewModelModule.kt in this file add some comments

--- app/src/main/java/com/example/counterappwithdi/di/ViewModelModule.kt
+++ app/src/main/java/com/example/counterappwithdi/di/ViewModelModule.kt
@@ -1,13 +1,25 @@
 package com.example.counterappwithdi.di
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.counterappwithdi.viewmodel.MainViewModel
+import com.example.counterappwithdi.viewmodel.ViewModelFactory
+import com.example.counterappwithdi.viewmodel.ViewModelKey
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import dagger.multibindings.IntoMap
 
 @Module
-class ViewModelModule {
-    @Provides
-    fun providesViewModel():MainViewModel{
-        return MainViewModel()
-    }
+abstract class ViewModelModule {
+
+    @Binds
+    abstract fun getViewModelFactory(viewModelFactory: ViewModelFactory): ViewModelProvider.Factory
+    // Using this Module we will pass daggers map which has all the viewModels to the viewModel Factory
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainViewModel::class)
+    abstract fun getMainViewModel(mainViewModel: MainViewModel): ViewModel
+    //Here we are putting MainViewModel using the key we defined earlier into DaggersMap by annotation @IntoMap
 }
