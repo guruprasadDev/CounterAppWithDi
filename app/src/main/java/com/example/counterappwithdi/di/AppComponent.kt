e71b34c6f705237ef324e29c73310d1dfb26fbe9
guruprasadesambattu <guruprasad.esambattu@mutualmobile.com>
2023-01-17 10:46:54 +0530

    Add AndroidInjectionModule in @Component and add @Component.Factory inside create function

--- app/src/main/java/com/example/counterappwithdi/di/AppComponent.kt
+++ app/src/main/java/com/example/counterappwithdi/di/AppComponent.kt
@@ -1,11 +1,17 @@
 package com.example.counterappwithdi.di
 
-import com.example.counterappwithdi.MainActivity
-import com.example.counterappwithdi.viewmodel.MainViewModel
+import com.example.counterappwithdi.CounterApp
+import dagger.BindsInstance
 import dagger.Component
+import dagger.android.AndroidInjectionModule
 import dagger.android.AndroidInjector
+import javax.inject.Singleton
 
-@Component(modules = [ViewModelModule::class, ActivityModule::class])
-interface AppComponent {
-    fun mainActivityInject(activity: MainActivity)
+@Singleton
+@Component(modules = [AndroidInjectionModule::class, ActivityModule::class, AppModule::class,ViewModelModule::class])
+interface AppComponent : AndroidInjector<CounterApp> {
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance app: CounterApp): AppComponent
+    }
 }
